63333977c6ae6018d5732fe45b074ffce98de415
Rachel <racheljones3@comcast.net>
2023-05-02 04:42:07 +0000

    final edits

--- scripts.js
+++ scripts.js
@@ -107,6 +107,24 @@ let dinner3 = new Recipe("Lemon Chicken with Orzo", "dinner3.html");
 let dinner4 = new Recipe("Mediterranean Turkey Skillet", "dinner4.html");
 let dinner5 = new Recipe("Pesto Gnocchi Sheet Pan", "dinner5.html");
 
+let breakfast1 = new Recipe("Veggie Breakfast Sandwich", "breakfast1.html");
+let breakfast2 = new Recipe("Apple Cinnamon Baked Oatmeal", "breakfast2.html");
+let breakfast3 = new Recipe("Sausage Egg Casserole", "breakfast3.html");
+let breakfast4 = new Recipe("Brussels Sprouts and Bacon Hash with Eggs", "breakfast4.html");
+let breakfast5 = new Recipe("French Toast Casserole", "breakfast5.html");
+
+let lunch1 = new Recipe("BLT Lettuce Wraps with Avocado", "lunch1.html");
+let lunch2 = new Recipe("Kale Salad with Lemon Dressing", "lunch2.html");
+let lunch3 = new Recipe("Pizza Twists", "lunch3.html");
+let lunch4 = new Recipe("Avocado Pizza", "lunch4.html");
+let lunch5 = new Recipe("Ranch Chicken Salad", "lunch5.html");
+
+let snacks1 = new Recipe("Roasted Butternut Squash Seeds", "snacks1.html");
+let snacks2 = new Recipe("Peanut Butter Oat Energy Balls", "snacks2.html");
+let snacks3 = new Recipe("Ginger Beet Juice", "snacks3.html");
+let snacks4 = new Recipe("Garlic Hummus", "snacks4.html");
+let snacks5 = new Recipe("Strawberry Cucumber Juice", "snacks5.html");
+
 document.addEventListener("DOMContentLoaded", function()
 {
     dinner1.addToDinnerNavigation();
@@ -115,6 +133,24 @@ document.addEventListener("DOMContentLoaded", function()
     dinner4.addToDinnerNavigation();
     dinner5.addToDinnerNavigation();
   
+    breakfast1.addToBreakfastNavigation();
+    breakfast2.addToBreakfastNavigation();
+    breakfast3.addToBreakfastNavigation();
+    breakfast4.addToBreakfastNavigation();
+    breakfast5.addToBreakfastNavigation();
+    
+    lunch1.addToLunchNavigation();
+    lunch2.addToLunchNavigation();
+    lunch3.addToLunchNavigation();
+    lunch4.addToLunchNavigation();
+    lunch5.addToLunchNavigation();
+    
+    snacks1.addToSnacksNavigation();
+    snacks2.addToSnacksNavigation();
+    snacks3.addToSnacksNavigation();
+    snacks4.addToSnacksNavigation();
+    snacks5.addToSnacksNavigation();
+    
 });
 document.getElementById("logos").addEventListener("click", function()
 {
